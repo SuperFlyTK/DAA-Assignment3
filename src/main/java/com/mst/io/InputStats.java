9980a8ae3139b47661d0f73cb085885856ab7742
ERA <rnesitajlo@gmail.com>
2025-10-26 01:06:43 +0500

    feat: add original test data and JSON utilities (upd)

---
+++ src/main/java/com/mst/io/InputStats.java
@@ -0,0 +1,19 @@
+package com.mst.io;
+
+public class InputStats {
+    private int vertices;
+    private int edges;
+
+    public InputStats() {}
+
+    public InputStats(int vertices, int edges) {
+        this.vertices = vertices;
+        this.edges = edges;
+    }
+
+    public int getVertices() { return vertices; }
+    public void setVertices(int vertices) { this.vertices = vertices; }
+
+    public int getEdges() { return edges; }
+    public void setEdges(int edges) { this.edges = edges; }
+}
